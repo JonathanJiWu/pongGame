203b9fb6651b9fd7ef0bc7b1f71374191ee54a44
JonathanJiWu <jonathanwubrave@gmail.com>
2022-08-26 16:47:33 -0400

    VIP

--- main.cpp
+++ main.cpp
@@ -15,11 +15,11 @@ enum EnumerationOfDirections
 class Ball
 {
 private:
-	int x, y;
-	int originalX, originalY;
+	int x, y;//store pos of the ball
+	int originalX, originalY;//initial positions
 	EnumerationOfDirections direction;
 public:
-	Ball(int posX, int posY)
+	Ball(int posX, int posY)//constructor(set initial X, and Y, also current X, Y)
 	{
 		originalX = posX;
 		originalY = posY;
@@ -37,16 +37,17 @@ public:
 	{
 		direction = dir;
 	}
+
 	inline int GetX() { return x; }
 	inline int GetY() { return y; }
 	inline int GetDir() { return direction; }
 
 	void RandomDirection()
 	{
-		direction = ( EnumerationOfDirections ) ((rand() % 6) + 1);
+		direction = ( EnumerationOfDirections ) ((rand() % 6) + 1);//cast the int into enum; rand num from 1 ~ 6
 	}
 
-	void Move()
+	void Move()//ball doesn't go straight left and right
 	{
 		switch (direction)
 		{
@@ -74,13 +75,36 @@ public:
 			x++;
 			y++;
 			break;
+		default:
+			break;
 		}
 	}
-	
+	friend ostream& operator<<(ostream& o, Ball c)
+	{
+		o << "Ball [" << c.x << "," << c.y << "]"<< "["<< c.direction << "]";
+		return o;
+	}
 };
 
 int main()
 {
-	
+	Ball c(0, 0);
+	cout << c << endl;
+
+	c.RandomDirection();
+	cout << c << endl;
+
+	c.Move();
+	cout << c << endl;
+
+	c.RandomDirection();
+	c.Move();
+	cout << c << endl;
+
+	c.RandomDirection();
+	c.Move();
+	cout << c << endl;
+
+
 	return 0;
 }
